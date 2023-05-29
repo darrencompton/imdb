a9a76546e102169270b35e86d0a2f21d4c2f16b8
darrencompton <hacking@coolbyte.co.uk>
2023-05-29 12:42:16 +0100

    Added get Trended and Top rated API Calls

---
+++ src/app/components/Results.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Results({results}) {
+  return (
+    <div>
+      Results
+      {results.map((result) =>(
+        <div key={result.id}>
+
+            {result.original_title}
+        </div>
+      ))}
+    </div>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,24 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Results from "./components/Results";
 
-export default function Home() {
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const fetchString = `https://api.themoviedb.org/3/${
+    genre === "fetchTopRated" ? "movie/top_rated" : "trending/movie/week"
+  }?api_key=${API_KEY}&language=en-us&page=1`;
+  const res = await fetch(fetchString, { next: { revalidate: 100 } });
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const data = await res.json();
+  const results = data.results;
+  console.log(results[0]);
+
   return (
-    <h1 className='text-red-500'>Home</h1>
-      )
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
